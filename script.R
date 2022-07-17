e68c3435a8207cc2e8c77f27f8869d054a49846f
sigma-dna <104036233+sigma-dna@users.noreply.github.com>
2022-07-17 13:08:23 -0300

    correção source

--- script.R
+++ script.R
@@ -1,7 +1,7 @@
 source('./import.R') #importação
 source('./rel_municipio_variables.R') #tópico 2
 source('./rel_idade_escalas.R') #tópico 3
-source('./caracteriza_6_grupos.R.R') #tópico 4
+source('./caracteriza_6_grupos.R') #tópico 4
 source('./rel_discriminacao_violencia_in_mora.R') #tópico 5
 
 #Essas linhas são pra evitar incompatibilidade entre os pacotes readr e expss, NÃO APAGAR
